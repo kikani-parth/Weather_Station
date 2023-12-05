9ce528747b96a92c680672860465a7bd3a905e70
Neel Patel <neelp@metropolia.fi>
2023-12-05 15:26:37 +0200

    - Updated all the files according to the requirnment

--- Static/mqtt.js
+++ Static/mqtt.js
@@ -1,61 +1,62 @@
+// app.js
 // For this example, we'll use dummy data.
 const dummyData = {
-    liveData: 0,
-    liveChartData: [],
-    dateRangeChartData: [],
-  };
-  
-  // Dummy function to simulate MQTT data reception
-  function subscribeToMqtt() {
-    // Use MQTT library to subscribe to topics and update dummyData
-    // For example, subscribe to 'liveData' and 'liveChartData' topics
-    // When data is received, update dummyData and call updateUI function
-  }
-  
-  function updateUI() {
-    // Update live data display
-    document.getElementById('live-data').innerText = `Live Data: ${dummyData.liveData}`;
-  
-    // Update live chart
-    updateLiveChart();
-  
-    // Update date range chart
-    updateDateRangeChart();
-  }
-  
-  function updateLiveChart() {
-    const ctx = document.getElementById('live-chart').getContext('2d');
-    const liveChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: Array.from({ length: dummyData.liveChartData.length }, (_, i) => i + 1),
-        datasets: [{
-          label: 'Live Data',
-          data: dummyData.liveChartData,
-          borderColor: 'blue',
-          fill: false,
-        }],
-      },
-    });
-  }
-  
-  function updateDateRangeChart() {
-    // Implement logic to filter date range data and update the chart
-    const ctx = document.getElementById('date-range-chart').getContext('2d');
-    const dateRangeChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: [], // Date range labels
-        datasets: [{
-          label: 'Date Range Data',
-          data: [], // Filtered date range data
-          borderColor: 'green',
-          fill: false,
-        }],
-      },
-    });
-  }
-  
-  // Subscribe to MQTT topics and update UI
-  subscribeToMqtt();
-  
+  liveTemperature: 25, // Replace with actual live temperature data
+  liveHumidity: 50, // Replace with actual live humidity data
+  livePressure: 1013.25, // Default pressure value
+  liveAltitude: 0, // Default altitude value
+  liveChartData: [10, 20, 30, 25, 35], // Replace with actual live chart data
+  dateRangeChartData: [15, 25, 20, 30, 40], // Replace with actual date range chart data
+};
+
+// Replace 'YOUR_OPENWEATHERMAP_API_KEY' and 'YOUR_CITY' with actual values
+const apiKey = 'YOUR_OPENWEATHERMAP_API_KEY';
+const city = 'YOUR_CITY';
+
+function subscribeToMqtt() {
+  // ... (existing MQTT subscription logic)
+
+  // Fetch weather data from OpenWeatherMap API
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+      // Update dummyData with pressure and altitude data
+      dummyData.liveTemperature = data.main.temp; // Assuming temperature is in the response
+      dummyData.liveHumidity = data.main.humidity; // Assuming humidity is in the response
+      dummyData.livePressure = data.main.pressure;
+      dummyData.liveAltitude = calculateAltitude(data.main.pressure);
+
+      // Call updateUI to refresh the UI with new data
+      updateUI();
+    })
+    .catch(error => console.error('Error fetching weather data:', error));
+}
+
+function updateUI() {
+  // ... (existing UI update logic)
+
+  // Update pressure and altitude in the live data display
+  document.getElementById('live-temperature').innerText = `Temperature: ${dummyData.liveTemperature} °C`;
+  document.getElementById('live-humidity').innerText = `Humidity: ${dummyData.liveHumidity}%`;
+  document.getElementById('live-pressure').innerText = `Pressure: ${dummyData.livePressure} hPa`;
+  document.getElementById('live-altitude').innerText = `Altitude: ${dummyData.liveAltitude} meters`;
+}
+
+function updateLiveChart() {
+  // ... (existing live chart update logic)
+}
+
+function updateDateRangeChart() {
+  // ... (existing date range chart update logic)
+}
+
+function calculateAltitude(pressure) {
+  // Implement your altitude calculation logic here
+  // This is a simple example, you may need to use a more complex formula
+  const seaLevelPressure = 1013.25; // Standard sea level pressure in hPa
+  const altitude = 44330 * (1 - Math.pow(pressure / seaLevelPressure, 0.1903));
+  return altitude.toFixed(2); // Round to 2 decimal places
+}
+
+// Subscribe to MQTT topics and update UI
+subscribeToMqtt();
